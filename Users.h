bf0152ad5b4b8b1c35a2f05d678338393840de13
Seuko <francisco.contreras.ra@gmail.com>
2025-08-21 16:46:57 -0400

    feat(users) : implementar creacion y listado de users

---
+++ Users.h
@@ -0,0 +1,15 @@
+//
+// Created by Francisco Contreras on 21-08-25.
+//
+#pragma once
+#include<string>
+#ifndef OS_USERS_H
+#define OS_USERS_H
+using namespace std;
+struct User {
+    string nombre;
+    int id;
+    int perfil;
+
+};
+#endif //OS_USERS_H
--- main.cpp
+++ main.cpp
@@ -1,32 +1,65 @@
 #include <iostream>
+#include <limits>
+#include "Users.h"
+using namespace std;
+
+void agregarUser(vector<User>& lista);
+void listarUsers(const vector<User>& lista);
 
 int main() {
+    vector<User> lista_users;
     int opcion = 0;
-
     while (true) {
-        std::cout << "\n===== MENU =====" << std::endl;
-        std::cout << "1. Opcion 1" << std::endl;
-        std::cout << "2. Opcion 2" << std::endl;
-        std::cout << "3. Opcion 3" << std::endl;
-        std::cout << "0. Salir" << std::endl;
-        std::cout << "Elige una opcion: ";
-        std::cin >> opcion;
+        cout << "\n===== MENU =====" << endl;
+        cout << "1. Modulo Admin" << endl;
+        cout << "2. Modulo General" << endl;
+        cout << "3. Modulo por implementar " << endl;
+        cout << "0. Salir" << endl;
+        cout << "Elige una opcion: ";
+        while (!(cin >> opcion)) {
+            cout << "Entrada no valida, Intenta denuevo" << endl;
+            cin.clear(); // limpia estado de error
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // limpia buffer de cin
+        }
 
-        switch (opcion) {
-            case 1:
-                std::cout << "Elegiste la opcion 1" << std::endl;
-                break;
-            case 2:
-                std::cout << "Elegiste la opcion 2" << std::endl;
-                break;
-            case 3:
-                std::cout << "Elegiste la opcion 3" << std::endl;
-                break;
-            case 0:
-                std::cout << "Saliendo del programa..." << std::endl;
-                return 0;
-            default:
-                std::cout << "Opcion invalida, intenta de nuevo." << std::endl;
+        if (opcion == 0) {
+            cout << "Saliendo del programa..." << endl;
+            break;
+        }
+        if (opcion == 1) {
+            int admin_opcion = 0;
+            do {
+                cout << "\n===== Modulo ADMIN =====" << endl;
+                cout << "1. Crear Usuario" << endl;
+                cout << "2. Eliminar Usuario" << endl;
+                cout << "3. Listar Usuarios" << endl;
+                while (!(cin >> admin_opcion)) {
+                    cout << "Entrada no valida, Intenta denuevo" << endl;
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }
+                if (admin_opcion == 1) {
+                    char continuar = 'n';
+                    do{
+                        agregarUser(lista_users);
+                        cout << "Desea agregar otro usuario? (s/n): ";
+                        cin >> continuar;
+                    } while (continuar == 's' || continuar == 'S');
+                }
+                if (admin_opcion == 2) {
+                    // por implementar
+                }
+                if (admin_opcion == 3) {
+                    listarUsers(lista_users);
+                }
+            }while (admin_opcion != 0);
+
+        }
+        if (opcion == 2) {
+            // user
+        }
+        if (opcion == 3) {
+            //to do
         }
     }
 }
