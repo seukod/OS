7d92199328c0c1d3098cbeffda4bf79c183183db
panxuloo <franhernandez651@gmail.com>
2025-08-27 12:51:48 -0400

    build para make

---
+++ build/CMakeFiles/OS.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pancho/Desktop/SEXTO_SEMESTRE/SISTEMAS_OPERATIVOS/repo_github_proyecto/entrega_1/OS/Users.cpp" "CMakeFiles/OS.dir/Users.cpp.o" "gcc" "CMakeFiles/OS.dir/Users.cpp.o.d"
+  "/home/pancho/Desktop/SEXTO_SEMESTRE/SISTEMAS_OPERATIVOS/repo_github_proyecto/entrega_1/OS/main.cpp" "CMakeFiles/OS.dir/main.cpp.o" "gcc" "CMakeFiles/OS.dir/main.cpp.o.d"
+  "" "OS" "gcc" "CMakeFiles/OS.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/OS.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS.dir/link.d"
+  "CMakeFiles/OS.dir/Users.cpp.o"
+  "CMakeFiles/OS.dir/Users.cpp.o.d"
+  "CMakeFiles/OS.dir/main.cpp.o"
+  "CMakeFiles/OS.dir/main.cpp.o.d"
+  "OS"
+  "OS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
