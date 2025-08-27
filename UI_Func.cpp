f5d4a7d3bf2f51615cb938c7f17cca73a3ac8ce0
Seuko <francisco.contreras.ra@gmail.com>
2025-08-27 14:14:37 -0400

    fix: borrao de todo

---
+++ UI_Func.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Francisco Contreras on 27-08-25.
+//
+
+#include "UI_Func.h"
---
+++ UI_Func.h
@@ -0,0 +1,8 @@
+//
+// Created by Francisco Contreras on 27-08-25.
+//
+
+#ifndef OS_UI_FUNC_H
+#define OS_UI_FUNC_H
+
+#endif //OS_UI_FUNC_H
